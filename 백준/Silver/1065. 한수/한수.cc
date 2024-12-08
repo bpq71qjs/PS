1fe4d3bae0f336857637b434d9953589b7227004
bpq71qjs <bpq71qjs@naver.com>
2024-12-08 17:27:28 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int f(int num) {
+	int cnt = 0;
+ 
+	if (num < 100) {
+		return num;
+	} else {
+		cnt = 99;
+ 
+		for (int i = 100; i <= num; i++) {
+			int hun = i / 100;	
+			int ten = (i / 10) % 10;
+			int one = i % 10;
+ 
+			if ((hun - ten) == (ten - one)) { 
+				cnt++;
+			}
+		}
+	}
+	return cnt;
+}
+ 
+int main(int argc, char const *argv[]) {
+ 
+	int N;
+	cin >> N;
+ 
+	int result = f(N);
+	cout << result;
+	return 0;
+}
